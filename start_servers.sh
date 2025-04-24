72974d80e6633ddaca6f79635384d1bb172e536e
GoncaloP0710 <goncalopinto07102003@gmail.com>
2025-04-24 23:24:44 +0100

    package updates

--- start_servers.sh
+++ start_servers.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 echo "Starting BattleServer..."
-kitty --hold zsh -c "cd Back-End/Honkai-StarBucks_BattleServer && node app.js" &
+kitty --hold zsh -c "cd Back-End/Honkai-StarBucks_BattleServer && npm install && node app.js" &
 
 echo "Starting LoginServer..."
-kitty --hold zsh -c "cd Back-End/Honkai-StarBucks_LoginServer && node app.js" &
+kitty --hold zsh -c "cd Back-End/Honkai-StarBucks_LoginServer && npm install && node app.js" &
 
 echo "Starting MainServer..."
-kitty --hold zsh -c "cd Back-End/Honkai-StarBucks_MainServer && node app.js" &
+kitty --hold zsh -c "cd Back-End/Honkai-StarBucks_MainServer && npm install && node app.js" &
 
 echo "Starting TrailBlazersServer..."
-kitty --hold zsh -c "cd Back-End/Honkai-StarBucks_TrailBlazersServer && node app.js" &
+kitty --hold zsh -c "cd Back-End/Honkai-StarBucks_TrailBlazersServer && npm install && node app.js" &
 
 echo "Starting Front-End..."
-kitty --hold zsh -c "cd Front-End/Honkai-StarBucks_Client && ng serve" &
+kitty --hold zsh -c "cd Front-End/Honkai-StarBucks_Client && npm install && ng serve" &
 
 echo "Waiting for Front-End to start..."
 
@@ -30,3 +30,4 @@ echo "Opening Front-End in the default web browser..."
 xdg-open http://localhost:4200
 
 echo "All servers started."
+```
