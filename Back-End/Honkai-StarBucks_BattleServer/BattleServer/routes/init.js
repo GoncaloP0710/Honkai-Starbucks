dd02e7ba54ff8bf1dd1aaad8ec25aaa02a058228
GoncaloP0710 <goncalopinto07102003@gmail.com>
2025-04-24 23:03:57 +0100

    Node modules removed and more structure changes

--- Back-End/Honkai-StarBucks_BattleServer/BattleServer/routes/init.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const initController = require('../controller/InitController');
-
-router.post('/', initController.init);
-
-module.exports = router;
